9ba4a649d0d5cd8c48cc395d532e5c16ee689f59
Kush <kushsrigiri@gmail.com>
2024-12-06 14:30:06 +0100

    task completet

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -3,6 +3,14 @@ package main
 import "fmt"
 
 func main() {
+	firstName := "John"
+	lastName := "Doe"
+	dayOfBirth := 15
+	monthOfBirth := 10
+	yearOfBirth := 1995
+	numberOfSiblings := 2
+	heightInMeters := 1.80
+	zodiacSign := '\u264b' // TODO: calculate using the provided formula!
 	// TODO: Declare and initialize the variables being used in the output!
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
